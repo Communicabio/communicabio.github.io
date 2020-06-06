64a73d972665936a3a347ed1859b12e3a24b61a1
Andrew Ishutin <hazmozavr@gmail.com>
2020-06-06 18:01:32 +0300

    auth homepage fix

---
+++ build/precache-manifest.8b7ab58c22b2bafbafc398852e903737.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2574830fbc9b6d570f1cd274c534404",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "91716ff013e3c23d9ddf",
+    "url": "/build/static/css/2.ad8c1e94.chunk.css"
+  },
+  {
+    "revision": "91716ff013e3c23d9ddf",
+    "url": "/build/static/js/2.432cbd31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/build/static/js/2.432cbd31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e201232436d1c7d49807",
+    "url": "/build/static/js/main.fd54a99c.chunk.js"
+  },
+  {
+    "revision": "58cb39f67bf0215772f9",
+    "url": "/build/static/js/runtime-main.03e1dfb9.js"
+  },
+  {
+    "revision": "e8edc935f97733a0a6c0faacca73aa37",
+    "url": "/build/static/media/communicabio.e8edc935.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5e3b42efdcb23a8a9566ee9ee6c6611d.js"
+  "/build/precache-manifest.8b7ab58c22b2bafbafc398852e903737.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
