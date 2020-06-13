2f23c6bbfb985ba9f71d99a3b5e836a7e65ebcdb
Andrew Ishutin <hazmozavr@gmail.com>
2020-06-13 17:34:51 +0300

    share

---
+++ share/precache-manifest.6700ac1ef04678c3b0c98f1ab9d7833a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8edc935f97733a0a6c0faacca73aa37",
+    "url": "/share/static/media/communicabio.e8edc935.svg"
+  },
+  {
+    "revision": "6ff70aa9faff40599116",
+    "url": "/share/static/js/runtime~main.a9633473.js"
+  },
+  {
+    "revision": "a6e78577e3040dc26322",
+    "url": "/share/static/js/main.18e14136.chunk.js"
+  },
+  {
+    "revision": "12e7ef1de529d7b55395",
+    "url": "/share/static/js/2.2247176b.chunk.js"
+  },
+  {
+    "revision": "12e7ef1de529d7b55395",
+    "url": "/share/static/css/2.548d1aff.chunk.css"
+  },
+  {
+    "revision": "9be058ca0fb334433b69fa969418524f",
+    "url": "/share/index.html"
+  }
+];
---
+++ share/vk_script.js
@@ -0,0 +1,1 @@
+VK.Widgets.Auth("vk_auth", {"onAuth":function(data) {alert('user '+data['uid']+' authorized');}});
